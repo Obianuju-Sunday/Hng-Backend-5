8f1f408b99a2812a171e21338f90fdc985e3b7ca
Obianuju-Sunday <obianujusunday43@gmail.com>
2023-10-02 19:06:16 +0100

    new changes

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
  
-app.use('/api', routes);
+app.use(
+    '/api', routes);
 
 app.listen(3000, () =>{
     console.log(`Server started at ${3000}`)
