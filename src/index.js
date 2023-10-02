e12d2bb67a77ff3459322cdb31639e64d9e8fdef
Obianuju-Sunday <obianujusunday43@gmail.com>
2023-10-02 21:58:58 +0100

    new changes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,14 @@ database.once('connected', () => {
 })
 
 const app = express();
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500/popup.html',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204,
+};
+  
+app.use(cors(corsOptions))
 app.use(express.json());
  
 app.use(
